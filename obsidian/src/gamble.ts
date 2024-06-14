5423bb076529f5d1986a3948e27af0445122798d
notlelouch <notlelouchvb@gmail.com>
2024-06-14 13:38:53 +0530

    created and synced subgraph for indexing

--- obsidian/src/gamble.ts
+++ obsidian/src/gamble.ts
@@ -1,71 +1,46 @@
 import {
-  GameEnded as GameEndedEvent,
-  GameStarted as GameStartedEvent,
-  OwnershipTransferred as OwnershipTransferredEvent,
-  PlayerJoined as PlayerJoinedEvent
-} from "../generated/Gamble/Gamble"
-import {
   GameEnded,
   GameStarted,
   OwnershipTransferred,
   PlayerJoined
-} from "../generated/schema"
+} from "../generated/Gamble/Gamble"
+import {Game} from "../generated/schema"
 
-export function handleGameEnded(event: GameEndedEvent): void {
-  let entity = new GameEnded(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.gameId = event.params.gameId
-  entity.winner = event.params.winner
+export function handleGameEnded(event : GameEnded): void {
+  let entity = Game.load(event.params.gameId.toString());
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  if (!entity) {
+    return;
+  }
 
-  entity.save()
+  entity.winner = event.params.winner;
+  entity.save();
 }
 
-export function handleGameStarted(event: GameStartedEvent): void {
-  let entity = new GameStarted(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.gameId = event.params.gameId
-  entity.maxPlayers = event.params.maxPlayers
-  entity.entryFee = event.params.entryFee
+export function handleGameStarted(event : GameStarted): void {
+  let entity = Game.load(event.params.gameId.toString());
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  if (!entity) {
+    entity = new Game(event.params.gameId.toString());
+    entity.players  = [];
+  }
 
-  entity.save()
+  entity.maxPlayers = event.params.maxPlayers;
+  entity.entryFee = event.params.entryFee;
+  entity.save();
 }
 
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
+export function handlePlayerJoined(event : PlayerJoined): void {
+  let entity = Game.load(event.params.gameId.toString());
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  if (!entity) {
+    return;
+  }
 
-  entity.save()
+  let newPlayers = entity.players;
+  newPlayers.push(event.params.player);
+  entity.players = newPlayers;
+  entity.save();
 }
 
-export function handlePlayerJoined(event: PlayerJoinedEvent): void {
-  let entity = new PlayerJoined(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.gameId = event.params.gameId
-  entity.player = event.params.player
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
+export function handleOwnershipTransferred(event : OwnershipTransferred): void {}
