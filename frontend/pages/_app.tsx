9d4588a0039f210c3184ed446d153780052828fd
notlelouch <notlelouchvb@gmail.com>
2024-06-14 18:16:34 +0530

    Added frontend as a regular directory

---
+++ frontend/pages/_app.tsx
@@ -0,0 +1,35 @@
+import '../styles/globals.css';
+import '@rainbow-me/rainbowkit/styles.css';
+import type { AppProps } from 'next/app';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { WagmiProvider } from 'wagmi';
+import {
+  sepolia,
+} from 'wagmi/chains';
+import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+
+const config = getDefaultConfig({
+  appName: 'RainbowKit App',
+  projectId: 'a9c5c888f9a582ae113ebae76801c4a9',
+  chains: [
+    sepolia,
+  ],
+  ssr: true,
+});
+
+const client = new QueryClient();
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={client}>
+        <RainbowKitProvider>
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
+
+export default MyApp;
